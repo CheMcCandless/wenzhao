45fb4ae2e24b16a2869b5f7d09a8eeabbfe0b9e0
gfw-breaker <lixiaoming0513@gmail.com>
2018-10-12 07:57:44 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -72,7 +72,9 @@ for v in $(ls -t *.mp4); do
 <a href="http://$ip:10000/videos/blog/tuid.html"><b>三退大潮席卷全球 三亿人觉醒见证中共末日</b></a><br/><br/>
 <a href="http://$ip:10000/videos/jiuping/index.html"><b>《九评共产党》</b></a>&nbsp;&nbsp;
 <a href="http://$ip:10000/videos/mtdwh/index.html"><b>《漫谈党文化》</b></a>&nbsp;&nbsp;
-<a href="https://github.com/gfw-breaker/nogfw/blob/master/README.md" target="_blank"><b> 一键翻墙软件</b></a>
+<a href="https://github.com/gfw-breaker/nogfw/blob/master/README.md" target="_blank"><b> 一键翻墙软件</b></a><br/><br/>
+<a href="http://$ip:8000/xtr/gb/prog99.html"><b>《热点互动直播》</b></a>&nbsp;&nbsp;
+<a href="http://$ip:8000/xtr/gb/prog109.html"><b>《大陆新闻解读》</b></a><br/><br/>
 </center></p>
 EOF
 
