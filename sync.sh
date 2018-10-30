bc5a1c7fc5b362f2a76e79129e37c92595db6fd2
gfw-breaker <lixiaoming0513@gmail.com>
2018-10-30 22:45:25 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -85,6 +85,8 @@ for v in $(ls -t *.mp4); do
 <a href="http://$ip:10000/videos/blog/weihuo.html"><b>天安门自焚真相《伪火》</b></a>&nbsp;&nbsp;
 <a href="http://$ip:10000/videos/blog/425event.html"><b>“四‧二五”中南海万人上访始末</b></a>&nbsp;&nbsp;
 <br/><br/>
+<a href="http://$ip:10000/videos/res/DecadeOfInvestigation.html"><b>《活摘－十年调查 》:中国军医曝光大陆活摘器官一条龙黑幕</b></a>&nbsp;&nbsp;
+<br/><br/>
 <a href="http://$ip:10000/videos/res/DeathByChina.html"><b>中美贸易战“剧本” -《致命中国：美国如何失去制造业基础》</b></a>&nbsp;&nbsp;
 <br/><br/>
 <a href="http://$ip:10000/videos/res/pence.html"><b>“伐共宣言”- 美国副总统彭斯在哈德逊研究所演讲</b></a>&nbsp;&nbsp;
