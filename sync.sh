c196bf742e8e563732550713da615f4d36431f5a
gfw-breaker <lixiaoming0513@gmail.com>
2018-11-25 12:55:21 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -6,21 +6,13 @@ folder="wenzhao"
 stick="UefWtTqAH_M"
 youtube_url=https://www.youtube.com/channel/UCtAIPjABiQD3qjlEl1T5VpA
 dl_script=https://raw.githubusercontent.com/gfw-breaker/youtube-video/master/dl.sh
-md_page=/usr/share/nginx/html/$folder/index.md
 
 
 # download
 cd /root/$folder
 wget -q $dl_script -O dl.sh
-bash dl.sh $folder $youtube_url $stick
+bash dl.sh -f $folder -u $youtube_url -s $stick 
 
 
-# push
-cd /root/$folder
-git pull
-sed -i '5,$d' README.md
-cat $md_page >> README.md
-git commit -a -m 'ok'
-git push
 
 
