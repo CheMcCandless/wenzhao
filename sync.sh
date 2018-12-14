d5dc316074165e218542c590d5d3424435c0b66b
gfw-breaker <lixiaoming0513@gmail.com>
2018-12-14 15:20:59 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 # author: gfw-breaker
 
 folder="wenzhao"
-stick="_IyPfO7ESho"
+stick="Ld1qSi9m1wk"
 #stick="UefWtTqAH_M"
 youtube_url=https://www.youtube.com/channel/UCtAIPjABiQD3qjlEl1T5VpA
 dl_script=https://raw.githubusercontent.com/gfw-breaker/youtube-video/master/dl.sh
